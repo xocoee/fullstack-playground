9290a089825b751e0018063a238f928416468cd4
Vitalii Zakharchuk <xocoee@gmail.com>
2025-09-01 21:58:43 +0300

    Update fish.module.ts

--- backend/src/fish/fish.module.ts
+++ backend/src/fish/fish.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
 import { FishService } from './fish.service';
 import { FishController } from './fish.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Fish } from './entities/fish.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Fish])],
   controllers: [FishController],
   providers: [FishService],
 })
